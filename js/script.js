33329903f3f0e46c3df42773fccaacf93d52eefc
Владислав Шинкарьов <vshynkarov@e2r6p7.unit.ua>
2021-08-28 16:57:49 +0300

    class Calculator  get first function

--- js/script.js
+++ js/script.js
@@ -6,9 +6,40 @@ class Calculator {
         this.clearBtn = document.querySelector('.clear');
         this.changeBtn = document.querySelector('.change');
         this.result = 0;
-        this.equation = new String()
+        this.equation = ''
     };
-
+    calculateEquation() {
+        let array = this.equation.trim().split(' ')
+        let operators = ['/', '*', '+', '-'];
+        for (let operator of operators) {
+            while(array.includes(operator))  {
+                let firstItem = parseFloat(array[array.indexOf(operator)-1]);
+                let secondItem = parseFloat(array[array.indexOf(operator)+1]);
+                let result;
+                switch(operator) {
+                    case '*':
+                        result = parseFloat(firstItem * secondItem);
+                        break;
+                    case '/':
+                        result = parseFloat(firstItem / secondItem);
+                        break;
+                    case '-':
+                        result = parseFloat(firstItem - secondItem);
+                        break;
+                    case '+':
+                        result = parseFloat(firstItem + secondItem);
+                        break;
+                };
+                array.splice(array.indexOf(operator)-1, 1);
+                array.splice(array.indexOf(operator)+1, 1, result);
+                array.splice(array.indexOf(operator), 1);
+                
+            };
+        };
+        console.log(array[0])
+    };
 };
 
 const calculator = new Calculator();
+calculator.calculateEquation()
+
