480b1f51decbe991b2361dadd05d2e3bf8257000
Vasyl <gavronskiy70@gmail.com>
2021-08-28 13:43:41 +0300

    variables are created

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ class Calculator {
 
     }
 }
+
+const numberBtns = document.querySelectorAll('.number'),
+      operationBtns = document.querySelectorAll('.operation'),
+      equalsBtn = document.querySelector('.equals'),
+      clearBtn = document.querySelector('.clear'),
+      changeBtn = document.querySelector('.change');
+
+const calculator = new Calculator();
