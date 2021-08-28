59141987ea3de2283181407e07294f0dd092334b
Владислав Шинкарьов <vshynkarov@e2r6p7.unit.ua>
2021-08-28 14:27:50 +0300

    created class Calculator and builded constructor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 class Calculator {
     constructor() {
+        this.numberBtns = document.querySelectorAll('.number');
+        this.operationBtns = document.querySelectorAll('.operation');
+        this.equalsBtn = document.querySelector('.equals');
+        this.clearBtn = document.querySelector('.clear');
+        this.changeBtn = document.querySelector('.change');
+        this.result = 0;
+        this.equation = new String()
+    };
 
-    }
-}
-
-const numberBtns = document.querySelectorAll('.number'),
-      operationBtns = document.querySelectorAll('.operation'),
-      equalsBtn = document.querySelector('.equals'),
-      clearBtn = document.querySelector('.clear'),
-      changeBtn = document.querySelector('.change');
+};
 
 const calculator = new Calculator();
